3f1fb00d075d79e2b1ba4b8fbc1864a9eab68aa7
Alexander Reisner <ga67wel@mytum.de>
2017-07-18 10:22:51 +0200

    Fix target.mk and run file for savm protobuf 3.3.0

--- tools/QEMU-SA-VM/src/app/savm/target.mk
+++ tools/QEMU-SA-VM/src/app/savm/target.mk
@@ -1,7 +1,8 @@
-#proto_cc := $(REP_DIR)/../protobuf/build/sensor.pb.cc
-proto_h := $(REP_DIR)/../protobuf/build/state.pb.h
+proto_cc := $(wildcard $(REP_DIR)/../protobuf/build/*.c)
+proto_h := $(REP_DIR)/../protobuf/build
 TARGET = savm
-SRC_CC = main.cpp tcp_socket.h $(proto_h) #$(proto_cc)
-LIBS   = base libmosquitto stdcxx lwip pthread
+SRC_CC = main.cpp $(proto_cc) tcp_socket.cc
+LIBS   = base libmosquitto stdcxx lwip pthread libprotobuf
 
 INC_DIR += $(call select_from_repositories,include/lwip)
+INC_DIR += $(proto_h) $(REP_DIR)
