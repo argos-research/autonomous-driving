fbf2d7ef83ee8af2e2d823c78d906eeed0dee4b3
Alexander Reisner <ga67wel@mytum.de>
2017-07-23 10:31:37 +0200

    Consitency

--- tools/QEMU-SA-VM/src/app/savm/target.mk
+++ tools/QEMU-SA-VM/src/app/savm/target.mk
@@ -1,7 +1,7 @@
 proto_cc := $(wildcard $(REP_DIR)/../protobuf/build/*.pb.cc)
 proto_h := $(REP_DIR)/../protobuf/build
 TARGET = savm
-SRC_CC = main.cpp $(proto_cc) tcp_socket.cc
+SRC_CC = main.cc $(proto_cc) tcp_socket.cc
 LIBS   = base libmosquitto stdcxx lwip pthread libprotobuf
 
 INC_DIR += $(call select_from_repositories,include/lwip)
