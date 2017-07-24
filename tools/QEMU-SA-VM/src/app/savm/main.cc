595726b66508778b8c20509b5c1a4f124a5eed29
Alexander Reisner <ga67wel@mytum.de>
2017-07-24 16:11:34 +0200

    Publish any state info from Alex protobuf to Alex mosquitto server

--- tools/QEMU-SA-VM/src/app/savm/main.cc
+++ tools/QEMU-SA-VM/src/app/savm/main.cc
@@ -65,12 +65,12 @@ void Publisher::on_error() {
 			PDBG("Error!");
 		}
 
-void Publisher::my_publish(int id, float value) {
+void Publisher::my_publish(const char* name, float value) {
 	char buffer[1024] = { 0 };
 	int i = 0, ret = -1;
-	sprintf(buffer, "%d %f", id, value);
+	sprintf(buffer, "%s;%f", name, value);
 	ret = Publisher::publish(NULL, "state", strlen(buffer), buffer);
-	PDBG("state '%s' successful: %d", buffer, MOSQ_ERR_SUCCESS == ret);
+	//PDBG("state '%s' successful: %d", buffer, MOSQ_ERR_SUCCESS == ret);
 	i++;
 }
 
@@ -80,17 +80,17 @@ Proto_client::Proto_client() :
 	_target_addr{0}
 {
 	_in_addr.sin_family = AF_INET;
-	_in_addr.sin_port = htons(3001);
+	_in_addr.sin_port = htons(9002);
 
 	enum { BUF_SIZE = Nic::Packet_allocator::DEFAULT_PACKET_SIZE * 128 };
 
 	Genode::Xml_node network = Genode::config()->xml_node().sub_node("network");
 
-		char ip_addr[16] = {0};
+		char ip_addr[16] = {"10.0.0.2"};
 		char subnet[16] = {0};
 		char gateway[16] = {0};
 
-		network.attribute("ip-address").value(ip_addr, sizeof(ip_addr));
+		//network.attribute("ip-address").value(ip_addr, sizeof(ip_addr));
 		network.attribute("subnet-mask").value(subnet, sizeof(subnet));
 		network.attribute("default-gateway").value(gateway, sizeof(gateway));
 
@@ -103,7 +103,7 @@ Proto_client::Proto_client() :
 		}
 		PDBG("listen socket\n");
 
-		if (lwip_connect(_listen_socket, (struct sockaddr*)&_in_addr, sizeof(_in_addr)))
+		while (lwip_connect(_listen_socket, (struct sockaddr*)&_in_addr, sizeof(_in_addr)))
 		{
 			PERR("Connection failed!");
 			PERR("Reconnecting!");
@@ -127,21 +127,62 @@ void Proto_client::serve(Publisher *publisher)
 	float value=0;
 	while (true)
 	{
-		/*NETCHECK_LOOP(receiveInt32_t(size));
+		size=0;
+		//PDBG("loop\n");
+		NETCHECK_LOOP(receiveInt32_t(size));
 		if (size>0)
 		{
-			PDBG("Ready to receive state.");
-			Genode::Ram_dataspace_capability state_ds=Genode::env()->ram_session()->alloc(size);
-			protobuf::State* state=Genode::env()->rm_session()->attach(state_ds);
-			NETCHECK_LOOP(receive_data(state, size));
-			float spinVel=state->wheel(0).spinvel();
-			publisher->my_publish(0,0);
+			//PDBG("Ready to receive state.");
+			//PDBG("Got size %llu\n", ntohl(size));
+			Genode::Ram_dataspace_capability state_ds=Genode::env()->ram_session()->alloc(ntohl(size));
+			char* foo=Genode::env()->rm_session()->attach(state_ds);
+			NETCHECK_LOOP(receive_data(foo, ntohl(size)));
+			protobuf::State state;
+			state.ParseFromArray(foo,ntohl(size));			
+			float steer=state.steer();
+			publisher->my_publish("steer",steer);
+			float brakeCmd=state.brakecmd();
+			publisher->my_publish("brake",brakeCmd);		
+			float accelCmd=state.accelcmd();
+			publisher->my_publish("accel",accelCmd);
+			float spinVel0=state.wheel(0).spinvel();
+			publisher->my_publish("wheel0",spinVel0);
+			float spinVel1=state.wheel(1).spinvel();
+			publisher->my_publish("wheel1",spinVel1);
+			float spinVel2=state.wheel(2).spinvel();
+			publisher->my_publish("wheel2",spinVel2);
+			float spinVel3=state.wheel(3).spinvel();
+			publisher->my_publish("wheel3",spinVel3);
+			float length=state.specification().length();
+			publisher->my_publish("length",length);
+			float width=state.specification().width();
+			publisher->my_publish("width",width);
+			float wheelRadius=state.specification().wheelradius();
+			publisher->my_publish("wheelRadius",wheelRadius);
+			for(int i=0; i<state.sensor().size(); i++)
+			{
+				if(state.sensor(i).type()==protobuf::Sensor_SensorType_GPS)
+				{
+					float gps_x=state.sensor(i).value(0);
+					publisher->my_publish("gps_x",gps_x);
+					float gps_y=state.sensor(i).value(1);
+					publisher->my_publish("gps_y",gps_y);
+				}
+				else
+				{
+					float laser=state.sensor(i).value(0);
+					char buffer[1024] = { 0 };
+					sprintf(buffer, "laser%d", i);
+					const char* name=buffer;
+					publisher->my_publish(name,laser);
+				}
+			}
 			Genode::env()->ram_session()->free(state_ds);
 		}
 		else
 		{
 			PWRN("Unknown message: %d", size);
-		}*/
+		}
 	}
 }
 
@@ -209,7 +250,7 @@ int main(int argc, char* argv[]) {
 	mosquitto.attribute("port").value(port, sizeof(port));
 
 	PDBG("protobuf init");
-	Proto_client *client;
+	Proto_client *client = new Proto_client();
 	PDBG("done");
 
 	/* create new mosquitto peer */
