cfa07b5edad1d10d257480ce4808f0458b6ac20b
Alexander Weidinger <alexwegoo@gmail.com>
2017-08-05 20:33:08 +0200

    Fix #2.

--- ecu/autonomousParking/src/ecu/target.mk
+++ ecu/autonomousParking/src/ecu/target.mk
@@ -1,8 +1,8 @@
 TARGET = ecu
 
-SRC_CC += main.cc
-SRC_CC += Parking.cc $(REP_DIR)/../../tools/QEMU-SA-VM/src/app/savm/subscriber.cc $(REP_DIR)/../../tools/QEMU-SA-VM/src/app/savm/publisher.cc
-          
+SRC_CC += main.cc Parking.cc
+SRC_CC += $(addprefix $(REP_DIR)/../../tools/QEMU-SA-VM/src/app/savm/, subscriber.cc publisher.cc)
+
 LIBS   = base libmosquitto stdcxx lwip pthread
 
 INC_DIR += $(call select_from_repositories,include/lwip)
